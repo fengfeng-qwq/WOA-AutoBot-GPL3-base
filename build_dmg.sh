0dc804df3f56e7a7d4305f2422464b16169c28e4
fengeng_qwq <157942563+fengfeng-qwq@users.noreply.github.com>
2026-09-12 23:15:18 +0800

    chore: Update all version numbers to 1.5.10-beta

--- build_dmg.sh
+++ build_dmg.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 PYINST=.venv-1/bin/pyinstaller
 PY=.venv-1/bin/python
 APP_NAME="WOA_AutoBot"
-DMG_VOL="WOA AutoBot v1.5.0"
+DMG_VOL="WOA AutoBot v1.5.10-beta"
 
 echo ">>> [1/6] PyInstaller 打包..."
 rm -rf build dist
@@ -105,8 +105,8 @@ cat > "$APP/Contents/Info.plist" << PLIST
 <key>CFBundleName</key><string>WOA AutoBot</string>
 <key>CFBundleDisplayName</key><string>WOA AutoBot</string>
 <key>CFBundleIdentifier</key><string>com.woa.autobot</string>
-<key>CFBundleVersion</key><string>1.5.0</string>
-<key>CFBundleShortVersionString</key><string>1.5.0</string>
+<key>CFBundleVersion</key><string>1.5.10-beta</string>
+<key>CFBundleShortVersionString</key><string>1.5.10-beta</string>
 <key>CFBundlePackageType</key><string>APPL</string>
 <key>CFBundleExecutable</key><string>$APP_NAME</string>
 <key>CFBundleInfoDictionaryVersion</key><string>6.0</string>
